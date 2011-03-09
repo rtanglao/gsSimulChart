79d160941344c34fb7ab22ae9be8054deef81b32
Roland Tanglao <roland@rolandtanglao.com>
2011-03-09 08:15:05 -0800

    change back to 1 minute intervals and make new topic logs stand out

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var sys = require('sys'),
     gs = http.createClient(80, 'api.getsatisfaction.com'),
     gsPath = "/products/mozilla_thunderbird/topics.json?sort=recently_created&page=1&limit=30",
     chartId = "322dd869-0493-4485-b0b1-f64829508b1b",
-    interval = 5000,
+    interval = 60000,
     topicIds={};
 
 function postValue(id, value) {
@@ -44,7 +44,7 @@ function update() {
               if (topicIds[index] == null) {
                   numNewTopics++;
                   topicIds[index] = true;
-                  console.log("New topic id " + topic_id); 
+                  console.log("**New topic id " + topic_id); 
               }
               else {
                 console.log("Old topic id " + topic_id);
